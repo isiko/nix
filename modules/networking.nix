6f301d6a189ebe6d81d1441b3854fc829f1f473c
isiko <mail@isiko404.dev>
2023-12-20 15:21:34 +0100

    Lock down Ports

--- modules/networking.nix
+++ modules/networking.nix
@@ -11,30 +11,30 @@
     enable = true;
     checkReversePath = false; # Needed for VPN stuff
     allowedTCPPorts = [ 
-      4444
-      1312
-      3000  # Mister X
-      5173  # Mister X
-      57621 # Spotify
-      8384  # Syncthing
-      22000 # Syncthing
-      5037
+      #4444
+      #1312
+      #3000  # Mister X
+      #5173  # Mister X
+      #57621 # Spotify
+      #8384  # Syncthing
+      #22000 # Syncthing
+      #5037
     ];
     allowedUDPPorts = [
-      4444
-      1312
-      5353  # Spotify
-      3000  # Mister X
-      5173  # Mister X
-      22000 # Syncthing
-      21027 # Syncthing
+      #4444
+      #1312
+      #5353  # Spotify
+      #3000  # Mister X
+      #5173  # Mister X
+      #22000 # Syncthing
+      #21027 # Syncthing
     ];
     allowedTCPPortRanges = [
-      { from = 1714; to = 1764; } # KDE Connect
-      { from = 5500; to = 5599; } # ADB
+      #{ from = 1714; to = 1764; } # KDE Connect
+      #{ from = 5500; to = 5599; } # ADB
     ];
     allowedUDPPortRanges = [
-      { from = 1714; to = 1764; } # KDE Connect
+      #{ from = 1714; to = 1764; } # KDE Connect
     ];
   };
 }
